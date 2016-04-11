10865cc7a2cb82e9e6e449bbcbc4f4319980c73c
Christoffer Assgård <a.christoffer@outlook.com>
2016-04-11 10:49:29 +0200

    basic layout and with login

--- AgileProject/AgileProject/Controllers/HomeController.cs
+++ AgileProject/AgileProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,12 @@ namespace AgileProject.Controllers
     {
         public ActionResult Index()
         {
+
+            var user = User.Identity.Name;
+            if (user != "")
+            {
+                ViewBag.Message = "User";
+            }
             return View();
         }
 
