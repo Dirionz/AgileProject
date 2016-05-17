ecf3fff5fe87fa949ddbad572b4d80c262cf9b2b
Christoffer Assgård <a.christoffer@outlook.com>
2016-05-17 17:40:56 +0200

    Added initial test data in configuration

--- AgileProject/AgileProject/Migrations/Configuration.cs
+++ AgileProject/AgileProject/Migrations/Configuration.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using AgileProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace AgileProject.Migrations
 {
     using System;
@@ -15,18 +20,32 @@ namespace AgileProject.Migrations
 
         protected override void Seed(AgileProject.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var corridors = new List<Corridor>
+            {
+                new Corridor() { Name = "Corridor1"},
+                new Corridor() { Name = "Corridor2"},
+                new Corridor() { Name = "Corridor3"},
+            };
+            corridors.ForEach(s => context.Corridors.AddOrUpdate(p => p.Name, s));
+            context.SaveChanges();
+
+
+            if (!(context.Users.Any(u => u.UserName == "u@u.uu")))
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var userToInsert = new ApplicationUser { UserName = "u@u.uu", PhoneNumber = "0797697898" };
+                userManager.Create(userToInsert, "Hejhej123!");
+            }
+
+
+            var user = context.Users.FirstOrDefault(u => u.UserName == "u@u.uu");
+            var corridor = context.Corridors.FirstOrDefault(c => c.Name == "Corridor1");
+            var teacher = new Teacher() { FirstName = "Test", LastName = "Testsson", Phone = "0797697898", imageURL = "../images/default.jpg", isAdmin = true, Corridor = corridor, User = user};
+            context.Teacher.Add(teacher);
+            context.SaveChanges();
+
         }
     }
 }
